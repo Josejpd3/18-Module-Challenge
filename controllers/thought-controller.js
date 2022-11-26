cf7d881896b6662f7ab7de0cdbf27e66f9fea9eb
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 05:00:11 -0500

    thoughtController(removeReaction) in controllers/thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -91,6 +91,16 @@ const thoughtController = {
     },
 
     removeReaction(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+          )
+            .then((thought) =>
+              !thought
+                ? res.status(404).json({ message: 'Thought not found!' })
+                : res.status(200).json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
 }
