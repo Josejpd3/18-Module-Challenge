292b47b508010e70716264653a8b45510758609b
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 03:17:18 -0500

    userController(getSingleUser) in controllers/user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,4 +6,17 @@ const userController ={
           .then((users) => res.json(users))
           .catch((err) => res.status(500).json(err));
     },
+
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.UserId })
+        .select('-__v')
+        .populate('thoughts')
+        .populate('friends')
+        .then((User) =>
+          !User
+            ? res.status(404).json({ message: 'No User with that ID' })
+            : res.json(User)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 }
