50e3c84cbf1ec844dd1e2701dd2fd4692c1775ae
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 04:26:30 -0500

    thoughtController(getThoughts) in controllers/thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,9 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-    
+    getThoughts(req, res) {
+        Thought.find()
+          .then((thoughts) => res.json(thoughts))
+          .catch((err) => res.status(500).json(err));
+    },
 }
