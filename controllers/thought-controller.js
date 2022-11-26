de9ed16410a6ca4a75bb2770431611aa719c9968
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 04:27:23 -0500

    thoughtController(getSingleThought) in controllers/thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,9 +1,22 @@
-const { Thought, User } = require('../models');
+const { Thought, thought } = require('../models');
 
 const thoughtController = {
     getThoughts(req, res) {
         Thought.find()
           .then((thoughts) => res.json(thoughts))
           .catch((err) => res.status(500).json(err));
+    },
+
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .select('-__v')
+        .populate('thoughts')
+        .populate('friends')
+        .then((thought) =>
+          !thought
+            ? res.status(404).json({ message: 'No thought with that ID' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
     },
 }
