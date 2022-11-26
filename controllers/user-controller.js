317561c0ce0f83f115b2f89d621f6dda5bb62742
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 03:16:57 -0500

    userController(getUsers) in controllers/user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,9 @@
 const { User, Thought } = require('../models');
 
 const userController ={
-    
+    getUsers(req, res){
+        User.find()
+          .then((users) => res.json(users))
+          .catch((err) => res.status(500).json(err));
+    },
 }
