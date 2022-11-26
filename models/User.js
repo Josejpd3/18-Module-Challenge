c61e6dc009e7de538ef82303e4e405c1ee9e46d2
Jose Plasencia <josejpd3@gmail.com>
2022-11-25 21:58:02 -0500

    userSchema (user, email, thoughts, friends) in models/User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,35 @@
 const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        max_length: 16,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+);
