7965b4d1c8eb4ec19d24b74989bf5c5854369bc9
Jose Plasencia <josejpd3@gmail.com>
2022-11-25 22:55:16 -0500

    thoughtSchema in models/Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,25 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+}
+);
