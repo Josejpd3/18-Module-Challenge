5924c3f3166d1a180e5e88ab4b3ac096a8b2c07e
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 04:31:48 -0500

    thoughtController(createThought) in controllers/thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -19,4 +19,25 @@ const thoughtController = {
         )
         .catch((err) => res.status(500).json(err));
     },
+
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => {
+          return User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { new: true }
+          );
+        })
+        .then((user) =>
+          !user
+            ? res.status(404).json({
+              message: 'User not found',
+            })
+            : res.status(200).json('New thought')
+        )
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    },
 }
