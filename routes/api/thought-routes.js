80c5f73c472a8aa2fd463f99917bbb4385d84cb8
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 05:03:05 -0500

    fixed thought-controller require in routes/api/thought-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -7,7 +7,7 @@ const {
     deleteThought,
     addReaction,
     removeReaction,
-} = require('../../controllers/user-controller');
+} = require('../../controllers/thought-controller');
 
 router.route('/').get(getThoughts).post(createThought);
 
