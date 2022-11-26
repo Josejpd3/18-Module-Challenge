0104a08e2927895c158e5b52b32ffc105b93cd29
Jose Plasencia <josejpd3@gmail.com>
2022-11-26 03:25:32 -0500

    userController(removeFriend) in controllers/user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -64,4 +64,17 @@ const userController ={
             )
             .catch((err) => res.status(500).json(err));
     },
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+          )
+            .then((user) =>
+              !user
+                ? res.status(404).json({ message: 'No user with this id!' })
+                : res.status(200).json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 }
